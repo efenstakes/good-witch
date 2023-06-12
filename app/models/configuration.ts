575422f14b3b90dc85317ef0be4f7765ec3e47c5
efen stakes <efenstakes101@gmail.com>
2023-06-12 18:15:38 +0300

    add app models

---
+++ app/models/configuration.ts
@@ -0,0 +1,13 @@
+
+
+
+export interface IConfiguratorOption {
+    colors: Array<string>
+    title: string
+    selectedColor: string
+}
+
+export interface IConfiguration {
+    options: IConfiguratorOption
+    title: string
+}
