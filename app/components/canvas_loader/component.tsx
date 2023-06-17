171f72ae615e9d2f1353aef2c09164fcf6beea6b
efen stakes <efenstakes101@gmail.com>
2023-06-17 14:05:44 +0300

    add loader for 3d components

---
+++ app/components/canvas_loader/component.tsx
@@ -0,0 +1,30 @@
+import { Html, useProgress } from "@react-three/drei"
+
+const CanvasLoader = ()=> {
+    const { progress } = useProgress()
+
+    console.log("progress ", progress)
+
+    if( progress == 100 ) {
+        return (
+            <></>
+        )
+    }
+    return (
+        <group>
+            <Html
+                center
+                className='canvas_loader'
+            >
+                <h1>
+                    {progress} %
+                </h1>
+                <p>
+                    Loaded
+                </p>
+            </Html>
+        </group>
+    )
+}
+
+export default  CanvasLoader
